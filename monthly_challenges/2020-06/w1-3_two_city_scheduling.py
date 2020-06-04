55a56c418ffe30d430bf207ebf1fb76651c973bc
Matthew Willcockson <matthe@willcockson.family>
2020-06-04 12:30:30 -0500

    Still working on w1-3
    The general balancing algorithm I'm trying to implement is:
    
    - Assign candidates to each's most cost-effective city
    - Find any cities with an excess or dearth of candidates
    - Sort candidates in excess by the cost of flying them to dearth cities
    - Move candidates, one at a time, from excess to dearth
    - Each time a candidate is moved, recalculate dearth cities and resort candidates
    - When no more dearth, if any are still in excess, sort excess descending by count
    - Sort candidates in city with most excess by cost of flying them to any city not in excess
    - Move candidates one at a time, resorting after each move based on which cities are not in excess (i.e. count < average, not count < floor(average))

--- monthly_challenges/2020-06/w1-3_two_city_scheduling.py
+++ monthly_challenges/2020-06/w1-3_two_city_scheduling.py
@@ -43,46 +43,119 @@ Note:
 # costs = [[1, 10], [1, 10]]
 # Correct answer is 11
 
-from collections import Counter
-from typing import Dict, List, TypedDict
+from collections import Counter, deque
+from math import floor
+from random import randint
+from typing import Callable, Deque, Dict, List, Tuple, TypedDict
+from warnings import warn
 
 
 class Candidate(TypedDict):
     prices: List[int]
     city: int
+    index: int
 
 
 Candidates = Dict[int, Candidate]
 
-def balance(candidates: Candidates) -> None:
-    city_counts = Counter(candidates[candidate]["city"] for candidate in candidates)
-    ordered_cities = city_count.most_common()
-    if not ordered_cities:
-        return
-
-    counts = [count for (city, count) in ordered_cities]
-    if len(counts) == len(set(counts))
-        return
-
-    
 
 def candidate_cost(candidate: Candidate) -> int:
     return candidate["prices"][candidate["city"]]
 
 
+def next_best_choice(candidate: Candidate) -> int:
+    ordered_prices = sorted(candidate["prices"])
+    if len(ordered_prices) <= candidate["city"]:
+        return candidate["prices"][-1]
+    else:
+        return candidate["prices"].index(ordered_prices[candidate["city"] + 1])
+
+
+def sort_choices(candidate: Candidate, cities: List[int]) -> List[Tuple[int, int]]:
+    num_cities = len(candidate["prices"])
+    if any(map(lambda x: x > num_cities, cities)):
+        raise ValueError(f"Cities out of range: {cities}")
+
+    return sorted((candidate["prices"][city], city) for city in cities)
+
+def best_candidate(candidates: List[Candidate], cities: List[Tuple[int, int]]) -> Tuple[int, int]:
+    pass
+
+
+def balance(candidates: Candidates) -> None:
+    city_counts = Counter(candidates[candidate]["city"] for candidate in candidates)
+    ordered_cities = city_counts.most_common()
+    if not ordered_cities:
+        return
+
+    # If all are the same
+    counts = [count for (city, count) in ordered_cities]
+    if len(counts) == len(set(counts)):
+        return
+
+    students_per_city = len(candidates) / len(ordered_cities)
+    s_p_c = floor(students_per_city)
+    if s_p_c != students_per_city:
+        warn(f"The number of students doesn't divide cleanly into the number of cities")
+
+    excess = [
+        (city, count - s_p_c) for (city, count) in ordered_cities if count - s_p_c > 1
+    ]
+    excess_cities = [city for (city, count) in excess]
+    dearth = [
+        (city, s_p_c - count) for (city, count) in ordered_cities if s_p_c - count > 1
+    ]
+    excess_candidates: Deque[Candidate] = deque(
+        [
+            candidates[candidate]
+            for candidate in candidates
+            if candidates[candidate]["city"] in excess_cities
+        ]
+    )
+    for 
+
+
+def candidate_choices(costs: List[List[int]]) -> Candidates:
+    if not costs:
+        raise ValueError("Cannot use empty list")
+
+    candidates: Candidates = dict()
+    for candidate, prices in enumerate(costs):
+        min_cost = min(prices)
+        city = prices.index(min_cost)
+        candidates[candidate] = Candidate(
+            {"prices": prices, "city": city, "index": candidate}
+        )
+
+    balance(candidates)
+
+    return candidates
+
+
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        if not costs:
-            raise ValueError("Cannot use empty list")
-
-        candidates: Candidates = dict()
-        for candidate, prices in enumerate(costs):
-            min_cost = min(prices)
-            city = prices.index(min_cost)
-            candidates[candidate] = Candidate(
-                {"prices": prices, "city": city,}
-            )
-
-        balance(candidates)
+        candidates = candidate_choices(costs)
 
         return sum(map(candidate_cost, candidates.values()))
+
+
+# Testing
+rcost: Callable[[], int] = lambda: randint(1, 1000)
+rcosts: Callable[[int, int], List[List[int]]] = lambda x, y: [
+    [rcost() for i in range(y)] for j in range(x)
+]
+c = Solution().twoCitySchedCost
+print(
+    f"""
+{c([[1,10], [1,10]])}
+{c([1,10])}
+"""
+)
+for i in range(10):
+    print(
+        f"""
+costs: {(costs := rcosts(randint(2,4), randint(1,4)))}
+candidates: {(candidates := candidate_choices(costs))}
+total cost: {c(costs)}
+"""
+    )
