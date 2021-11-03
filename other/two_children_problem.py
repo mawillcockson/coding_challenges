2f293d15db26a8f624b6f2c193616e4a6a007981
Matthew Willcockson <matthew@willcockson.family>
2021-11-03 17:05:00 -0500

    feat(two_children_problem): add better explanation

--- other/two_children_problem.py
+++ other/two_children_problem.py
@@ -4,13 +4,15 @@ https://en.wikipedia.org/wiki/Boy_or_Girl_paradox
 Two people visit a store, and buy two items, one at a time.
 Both are equally likely to buy a bag of flour, or a bag of sugar, each time.
 
-You need to borrow some flour.
+You need to borrow some flour,
+so you ask both of them if they have a bag of flour you can use.
 
 Person A says one of the items they bought is flour, and that they used it.
 Person B says the first item they bought was flour, and that they used it.
 
 Which person is more likely to have an extra bag of flour?
 """
+from fractions import Fraction
 from random import randint
 
 NUMBER_OF_ITERATIONS = 10_000
@@ -54,7 +56,8 @@ def main(iterations: int = NUMBER_OF_ITERATIONS) -> None:
             else:
                 times_person_B_does_not_have_extra_flour += 1
 
-    print(f"out of {iterations} iterations:")
+    print(__doc__)
+    print(f"if this scenario happens {iterations:,} times, and you pick someone at random,")
 
     total_valid_person_A_scenarios = (
         times_person_A_has_extra_flour + times_person_A_does_not_have_extra_flour
@@ -71,6 +74,22 @@ def main(iterations: int = NUMBER_OF_ITERATIONS) -> None:
         times_person_B_has_extra_flour / total_valid_person_B_scenarios
     )
     print(f"person B had flour {percent_of_times_person_B_has_flour:.1%} of the time")
+
+    person_A_fraction = Fraction(
+        times_person_A_has_extra_flour, iterations
+    ).limit_denominator(iterations)
+    person_B_fraction = Fraction(
+        times_person_B_has_extra_flour, iterations
+    ).limit_denominator(iterations)
+    print(
+        f"""
+but if you don't ask beforehand, {person_A_fraction.numerator:,} times out of {person_A_fraction.denominator:,} ({float(person_A_fraction):.1%}) person A will have flour,
+and person B will have it {person_B_fraction.numerator:,} times out of {person_B_fraction.denominator:,} ({float(person_B_fraction):.1%})
+
+this is because out of {iterations:,} scenarios person A's statement was true in {total_valid_person_A_scenarios / iterations:.1%} of the scenarios,
+while person B's statement is true in only {total_valid_person_B_scenarios / iterations:.1%} of the scenarios.
+"""
+    )
 
 
 if __name__ == "__main__":
