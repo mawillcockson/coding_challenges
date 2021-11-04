419095f658713000dd247ea32630888f26327225
Matthew Willcockson <matthew@willcockson.family>
2021-11-03 22:43:05 -0500

    feat(two_children_problem): improve explanation and formatting

--- other/two_children_problem.py
+++ other/two_children_problem.py
@@ -1,13 +1,13 @@
 """
 https://en.wikipedia.org/wiki/Boy_or_Girl_paradox
 
-Two people visit a store, and buy two items, one at a time.
+Two people visit a store, and each buy two items, one at a time.
 Both are equally likely to buy a bag of flour, or a bag of sugar, each time.
 
-You need to borrow some flour,
-so you ask both of them if they have a bag of flour you can use.
+You need to borrow some flour.
+You ask both of them if they have a bag of flour you can use.
 
-Person A says one of the items they bought is flour, and that they used it.
+Person A says one of the items they bought was flour, and that they used it.
 Person B says the first item they bought was flour, and that they used it.
 
 Which person is more likely to have an extra bag of flour?
@@ -57,7 +57,11 @@ def main(iterations: int = NUMBER_OF_ITERATIONS) -> None:
                 times_person_B_does_not_have_extra_flour += 1
 
     print(__doc__)
-    print(f"if this scenario happens {iterations:,} times, and you pick someone at random,")
+    print(
+        f"""If they visit the store {iterations:,} times, and you pick someone at random
+each time they respond as in the scenario:
+"""
+    )
 
     total_valid_person_A_scenarios = (
         times_person_A_has_extra_flour + times_person_A_does_not_have_extra_flour
@@ -83,11 +87,12 @@ def main(iterations: int = NUMBER_OF_ITERATIONS) -> None:
     ).limit_denominator(iterations)
     print(
         f"""
-but if you don't ask beforehand, {person_A_fraction.numerator:,} times out of {person_A_fraction.denominator:,} ({float(person_A_fraction):.1%}) person A will have flour,
-and person B will have it {person_B_fraction.numerator:,} times out of {person_B_fraction.denominator:,} ({float(person_B_fraction):.1%})
+If you don't wait for a time when their answers are correct,
+{person_A_fraction.numerator:,} times out of every {person_A_fraction.denominator:,} visits to the store ({float(person_A_fraction):.1%}) person A will have flour,
+and person B will have it {person_B_fraction.numerator:,} times out of {person_B_fraction.denominator:,} visits ({float(person_B_fraction):.1%})
 
-this is because out of {iterations:,} scenarios person A's statement was true in {total_valid_person_A_scenarios / iterations:.1%} of the scenarios,
-while person B's statement is true in only {total_valid_person_B_scenarios / iterations:.1%} of the scenarios.
+This is because out of {iterations:,} scenarios, person A's statement was true in {total_valid_person_A_scenarios / iterations:.1%} of the scenarios,
+while person B's statement was true in only {total_valid_person_B_scenarios / iterations:.1%} of the scenarios.
 """
     )
 
