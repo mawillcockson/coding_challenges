0eddfe8bba99d27693fed659535f72442aa06756
Matthew Willcockson <matthew@willcockson.family>
2021-11-10 21:31:12 -0600

    feat(other): adjust other language examples

--- other/comparing_floats_2012/01_calculate_one.nim
+++ other/comparing_floats_2012/01_calculate_one.nim
@@ -1,10 +1,10 @@
 import strformat
 
-const f: float = 0.1
-var sum: float
+const f: float32 = 0.1
+var sum: float32
 sum = 0
 
 for i in 1..10:
   sum += f
-let product: float = f * 10
+let product: float32 = f * 10
 echo &"sum  = {sum:1.15f}\nmul1 = {product:1.15f}\nmul2 = {(block:f * 10):1.15f}"
--- other/comparing_floats_2012/01_calculate_one.py
+++ other/comparing_floats_2012/01_calculate_one.py
@@ -2,12 +2,15 @@
 from this article:
 https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
 """
+import sys
+
+MANTISSA_DIGITS = sys.float_info.mant_dig
 f: float = 0.1
 sum: float = 0
 
 for _ in range(10):
     sum += f
 product: float = f * 10
-print(f"""sum  = {sum:.15f}
-mul1 = {product:.15f}
-mul2 = {f * 10:.15f}""")
+print(f"""sum  = {sum:.{MANTISSA_DIGITS}f}
+mul1 = {product:.{MANTISSA_DIGITS}f}
+mul2 = {f * 10:.{MANTISSA_DIGITS}f}""")
