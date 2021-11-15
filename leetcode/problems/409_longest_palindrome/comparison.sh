27cc17a7af431bccac6d89be559e3977385d310b
Matthew Willcockson <matthew@willcockson.family>
2021-11-15 02:58:41 -0600

    leetcode(409): remove unused file

--- leetcode/problems/409_longest_palindrome/comparison.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-SETUP="a = list(range(2_000))"
-python -m timeit -s "${SETUP}" 'b = [*a, *a[::-1]]'
-python -m timeit -s "${SETUP}" 'b = a + a[::-1]'
-python -m timeit -s "${SETUP}" 'b = a[:];b.extend(a[::-1])'
-python -m timeit -s "${SETUP}" 'b = a[:];b.extend(reversed(a))'
