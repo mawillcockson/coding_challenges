0813ffc7694b78bfe011beb9e9dc09a993690b24
Matthew W <matthew@willcockson.family>
2024-09-18 18:36:36 -0500

    fix(ziglings): ..024

--- ziglings.org/exercises/023_errors3.zig
+++ ziglings.org/exercises/023_errors3.zig
@@ -12,14 +12,18 @@ const MyNumberError = error{TooSmall};
 
 pub fn main() void {
     const a: u32 = addTwenty(44) catch 22;
-    const b: u32 = addTwenty(4) ??? 22;
+    const b: u32 = addTwenty(4) catch |err| block: {
+        const out = std.io.getStdOut().writer();
+        out.print("{}\n", .{@TypeOf(err)}) catch unreachable;
+        break :block 22;
+    };
 
     std.debug.print("a={}, b={}\n", .{ a, b });
 }
 
 // Please provide the return type from this function.
 // Hint: it'll be an error union.
-fn addTwenty(n: u32) ??? {
+fn addTwenty(n: u32) MyNumberError!u32 {
     if (n < 5) {
         return MyNumberError.TooSmall;
     } else {
