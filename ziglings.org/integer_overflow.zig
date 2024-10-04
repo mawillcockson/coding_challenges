6bc7c93c9340488fad250685bb61cc1542b2b430
Matthew W <matthew@willcockson.family>
2024-10-04 12:29:22 -0500

    feat(zig): use builtin maxInt

--- ziglings.org/integer_overflow.zig
+++ ziglings.org/integer_overflow.zig
@@ -7,18 +7,16 @@ pub fn main() !void {
     var a: u2 = 0;
     try stdout.print("a starts with a value of -> {d}\n", .{a});
 
-    const Int = @typeInfo(@TypeOf(a)).int;
-    comptime if (Int.signedness != .unsigned) {
-        @compileError("Expected a to be an unsigned in");
-    };
     const max_int = comptime c: {
-        break :c pow(u16, 2, Int.bits) -| 1;
+        break :c std.math.maxInt(@TypeOf(a));
     };
     try stdout.print("a can hold a value up to {d}\n", .{max_int});
 
     inline for (0..max_int) |_| {
+        try stdout.print("add 1 to get -> {d}\n", .{a + 1});
         a += 1;
     }
+    try stdout.print("add 1 more to overflow\n", .{});
     a += 1;
 
     try stdout.print("a is -> {d}\n", .{a});
