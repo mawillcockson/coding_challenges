1fac1785d28041cbb453d46cff0bf7de7fcea326
Matthew W <matthew@willcockson.family>
2024-10-18 18:45:09 -0500

    fix(exercism/dart/armstrong-numbers): work for >64bit numbers

--- exercism/dart/armstrong-numbers/lib/armstrong_numbers.dart
+++ exercism/dart/armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,25 +1,23 @@
-import 'dart:math' as math;
-
 // I feel like this should be possible without resorting to BigInt?
+
+// Using BigInts in this version
 
 class ArmstrongNumbers {
   bool isArmstrongNumber(String number) {
     final int num_digits = number.length;
-    // I should've known that the values would overflow the range of 64 bit
-    // unsigned integers
-    final int original = int.parse(number, radix: 10);
-    List<int> nums = [];
-    int value = original;
-    while (value > 0) {
-      final int remainder = value % 10;
+    final BigInt original = BigInt.parse(number, radix: 10);
+    List<BigInt> nums = [];
+    BigInt value = original;
+    while (value > BigInt.from(0)) {
+      final remainder = value.remainder(BigInt.from(10));
       nums.add(remainder);
-      value ~/= 10;
+      value ~/= BigInt.from(10);
     }
-    int sum = nums
+    BigInt sum = nums
         // just hope it doesn't overflow (unless there's a way to check for
         // possible truncation?)
-        .map((x) => math.pow(x, nums.length).toInt())
-        .fold(0, (a, b) => (a + b) % original);
-    return sum == 0;
+        .map((BigInt x) => x.pow(nums.length))
+        .fold(BigInt.from(0), (BigInt a, BigInt b) => a + b);
+    return sum.compareTo(original) == 0;
   }
 }
