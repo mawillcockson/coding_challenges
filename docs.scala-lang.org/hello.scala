39bd6ca6cc78bc240722bd5b90ac0de9bef6c6ee
Matthew W <matthew@willcockson.family>
2025-03-20 17:53:58 -0500

    feat: add docs.scala-lang.org

---
+++ docs.scala-lang.org/hello.scala
@@ -0,0 +1,5 @@
+//> using scala 3.6.4
+
+@main
+def hello(name: String): Unit =
+  println(s"Hello, $name!")
---
+++ docs.scala-lang.org/tour/01.scala
@@ -0,0 +1,4 @@
+def why_multiple_paramater_lists(a: Int)(b: Int): Int = a + b
+
+@main def main(): Unit =
+  println(System.getProperty("user.name"))
