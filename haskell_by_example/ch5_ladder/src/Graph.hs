7a93ad9b862563e1c0fff69f7b8a3b15ecf356f7
Matthew W <matthew@willcockson.family>
2025-12-04 02:00:28 -0600

    feat(haskell/be/ch5): add alter function

--- haskell_by_example/ch5_ladder/src/Graph.hs
+++ haskell_by_example/ch5_ladder/src/Graph.hs
@@ -16,3 +16,13 @@ addNode :: Eq a => DiGraph a -> a -> DiGraph a
 addNode graph element
     | graph `hasNode` element = graph
     | otherwise = (element, []) : graph
+
+alter :: Eq k => (Maybe v -> Maybe v) -> k -> [(k, v)] -> [(k, v)]
+alter func key [] = case func Nothing of
+    Nothing -> []
+    Just value -> [(key, value)]
+alter func key ((key', value'):xs)
+    | key == key' = case func (Just value') of
+        Nothing -> xs
+        Just newValue -> (key', newValue) : xs
+    | otherwise = (key', value') : (alter func key xs)
