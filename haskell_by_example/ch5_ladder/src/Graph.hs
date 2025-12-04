f8b13c07495cb9a0d4c9ba94fa3740be5ce18a4c
Matthew W <matthew@willcockson.family>
2025-12-04 01:52:05 -0600

    feat(haskell/be/ch5): complete exercise rewrite member using lookup

--- haskell_by_example/ch5_ladder/src/Graph.hs
+++ haskell_by_example/ch5_ladder/src/Graph.hs
@@ -3,10 +3,7 @@ module Graph where
 type DiGraph a = [(a, [a])]
 
 member :: Eq a => a -> [(a, b)] -> Bool
-member _ [] = False
-member element ((x, _) : xs)
-    | x == element = True
-    | otherwise = member element xs
+member element lst = maybe False (const True) (lookup element lst)
 
 hasNode :: Eq a => DiGraph a -> a -> Bool
 hasNode = flip member
