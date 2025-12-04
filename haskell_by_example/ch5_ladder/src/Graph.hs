4d878bf744af6abf591b20c63b85901107f2c523
Matthew W <matthew@willcockson.family>
2025-12-04 01:41:05 -0600

    feat(haskell/be/ch5): add digraph functions

--- haskell_by_example/ch5_ladder/src/Graph.hs
+++ haskell_by_example/ch5_ladder/src/Graph.hs
@@ -7,3 +7,11 @@ member _ [] = False
 member element ((x, _) : xs)
     | x == element = True
     | otherwise = member element xs
+
+hasNode :: Eq a => DiGraph a -> a -> Bool
+hasNode = flip member
+
+addNode :: Eq a => DiGraph a -> a -> DiGraph a
+addNode graph element
+    | graph `hasNode` element = graph
+    | otherwise = (element, []) : graph
