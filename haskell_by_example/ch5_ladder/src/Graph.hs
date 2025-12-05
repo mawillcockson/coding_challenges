5611ba0c84278eeebe2f2d53b8d6aa7a2e2af057
Matthew W <matthew@willcockson.family>
2025-12-05 01:50:54 -0600

    fix(haskell/be/ch5): rewrite addNode to use AssocMap.alter; add delete{Edge,Node}

--- haskell_by_example/ch5_ladder/src/Graph.hs
+++ haskell_by_example/ch5_ladder/src/Graph.hs
@@ -5,22 +5,30 @@ module Graph (
     addEdge,
     addEdges,
     addNode,
+    children,
     buildDiGraph,
+    deleteEdge,
+    deleteNode,
     ) where
 
 import qualified Data.AssocMap as AM
 import qualified Data.Maybe (fromJust)
 import Data.Function ((&))
-import qualified Data.List (union, nub)
+import qualified Data.List (foldl', union, nub, (\\))
 
 type DiGraph a = AM.AssocMap a [a]
 
 main :: IO ()
 main = do
-    putStrLn $ "addEdges [(1,1),(2,2)] -> " ++ (addEdges [(1,1),(2,2)] (empty) & show)
-    putStrLn $ "addEdges [(1,1),(2,2),(2,3),(3,3),(3,2)] -> " ++ (addEdges [(1,1),(2,2),(2,3),(3,3),(3,2)] (empty) & show)
-    putStrLn $ "buildDiGraph [(1,[1]),(2,[2,3]),(3,[3,2])] -> " ++ (buildDiGraph [(1,[1]),(2,[2,3]),(3,[3,2])] & show)
-    putStrLn $ "buildDiGraph [(1,[1]),(2,[2,3]),(3,[3,2]),(1,[2]),(2,[2,3,3,4,4])] -> " ++ (buildDiGraph [(1,[1]),(2,[2,3]),(3,[3,2]),(1,[2]),(2,[2,3,3,4,4])] & show)
+    putStrLn $ "addEdges [(1,1),(2,2)] -> " ++ (addEdges [(1,1),(2,2)] (empty :: DiGraph Int) & show)
+    putStrLn $ "addEdges [(1,1),(2,2),(2,3),(3,3),(3,2)] -> " ++ (addEdges [(1,1),(2,2),(2,3),(3,3),(3,2)] (empty :: DiGraph Int) & show)
+    putStrLn $ "buildDiGraph [(1,[1]),(2,[2,3]),(3,[3,2])] -> " ++ ((buildDiGraph [(1,[1]),(2,[2,3]),(3,[3,2])] :: DiGraph Int) & show)
+    putStrLn $ "buildDiGraph [(1,[1]),(2,[2,3]),(3,[3,2]),(1,[2]),(2,[2,3,3,4,4]),(4,[4]),(5,[5])] -> " ++ ((buildDiGraph [(1,[1]),(2,[2,3]),(3,[3,2]),(1,[2]),(2,[2,3,3,4,4]),(4,[4]),(5,[5])] :: DiGraph Int) & show)
+    let example = buildDiGraph [(1,[1]),(2,[2,3]),(3,[3,2]),(1,[2]),(2,[2,3,3,4,4]),(4,[4]),(5,[5])] :: DiGraph Int
+    putStrLn "^ -> example"
+    putStrLn $ "deleteEdge example 1 2 -> " ++ (deleteEdge example 1 2 & show)
+    putStrLn $ "deleteEdge example 4 4 -> " ++ (deleteEdge example 4 4 & show)
+    putStrLn $ "deleteNode example 2 -> " ++ (deleteNode example 2 & show)
 
 empty :: DiGraph a
 empty = AM.empty
@@ -48,18 +56,29 @@ addEdge graph (startNode, endNode) =
 addEdge graph (startNode, endNode) = addNode graph (startNode, [endNode])
 
 addEdges :: Eq a => [(a, a)] -> DiGraph a -> DiGraph a
-addEdges edges graph = foldl' (addEdge) graph edges
+addEdges edges graph = Data.List.foldl' (addEdge) graph edges
 
 addNode :: Eq a => DiGraph a -> (a, [a]) -> DiGraph a
-addNode graph (startNode, endNodes) =
-    let intermediateGraph = newNode graph startNode
-        connectedNodes = AM.lookup intermediateGraph startNode & Data.Maybe.fromJust
-        modifiedConnections = Data.List.union connectedNodes endNodes
-        newGraph = AM.update intermediateGraph startNode modifiedConnections & Data.Maybe.fromJust
-    in newGraph
+addNode graph (startNode, endNodes) = AM.alter graph (insertNode) startNode where
+    insertNode Nothing = Just endNodes
+    insertNode (Just nodes) = Just $ Data.List.union nodes endNodes
 
 buildDiGraph :: Eq a => [(a, [a])] -> DiGraph a
-buildDiGraph = foldl' (addNode) (empty)
+buildDiGraph = Data.List.foldl' (addNode) (empty)
 
 children :: Eq a => DiGraph a -> a -> [a]
 children graph key = AM.findWithDefault graph [] key
+
+-- BUG: If the last connection is removed, should remove empty nodes
+deleteEdge :: Eq a => DiGraph a -> a -> a -> DiGraph a
+deleteEdge graph startNode endNode = AM.alter graph (aux) startNode where
+    aux Nothing = Nothing
+    aux (Just nodes) = Just $ nodes Data.List.\\ [endNode]
+
+-- BUG: actually need to remove node from every other node in the graph
+-- Because the graph is directed, just because one node is connected to
+-- another, doesn't mean that it will appear in the other node's connections
+deleteNode :: Eq a => DiGraph a -> a -> DiGraph a
+deleteNode graph startNode = AM.delete withoutNodeGraph startNode where
+    connectedNodes = AM.lookup graph startNode & Data.Maybe.fromJust
+    withoutNodeGraph = Data.List.foldl' (\graph' node -> deleteEdge graph' node startNode) graph connectedNodes
