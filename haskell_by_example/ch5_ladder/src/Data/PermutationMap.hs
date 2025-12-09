e93cb9ef3a65ef1a2673a7e43770f28ce4f95c02
Matthew W <matthew@willcockson.family>
2025-12-08 19:13:32 -0600

    feat(haskell/be/ch5): mirror AssocMap -> PermutationMap functions

--- haskell_by_example/ch5_ladder/src/Data/PermutationMap.hs
+++ haskell_by_example/ch5_ladder/src/Data/PermutationMap.hs
@@ -33,11 +33,17 @@ main = do
         example = empty & upsertF "abc" ["acc", "abb"] & upsertF "def" ["ddf"]
         ups = upsert example
         del = delete example
+        add = addWord example
+        adds = addWords example
     putStrLn $ "Example map: " ++ (show example)
-    putStrLn $ "Upsert abc [acc,adc] -> " ++ (ups "abc" ["acc","adc"] & show)
-    putStrLn $ "Upsert cab [adc,dbc] -> " ++ (ups "cab" ["adc","dbc"] & show)
+    putStrLn $ "Upsert cab [adc,dbc] -> " ++ (ups "cab" ["adc", "dbc"] & show)
     putStrLn $ "Delete abc -> " ++ (del "abc" & show)
     putStrLn $ "Delete hij -> " ++ (del "hij" & show)
+    putStrLn $ "addWords abc [acc,adc] -> " ++ (adds "abc" ["acc", "adc"] & show)
+    putStrLn $ "addWords cab [adc,dbc] -> " ++ (adds "cab" ["adc", "dbc"] & show)
+    putStrLn $ "addWord cab -> " ++ (add "cab" & show)
+    putStrLn $ "addWord dbc -> " ++ (add "dbc" & show)
+    putStrLn $ "createPermutationMap [abc,cab,bac,cad,dac,adc] -> " ++ (createPermutationMap ["abc", "cab", "bac", "cad", "dac", "adc"] & show)
 
 type PermutationMap = AM.AssocMap String [String]
 
@@ -68,11 +74,15 @@ lookup = flip $ flip AM.lookup . strSort
 findWithDefault :: PermutationMap -> [String] -> String -> [String]
 findWithDefault permutations defaultValue key = AM.findWithDefault permutations defaultValue $ strSort key
 
+addWords :: PermutationMap -> String -> [String] -> PermutationMap
+addWords graph word wrds = AM.alter graph (insertWord) $ strSort word
+  where
+    lowerCased = map strLower wrds
+    insertWord Nothing = Just lowerCased
+    insertWord (Just wrds') = Just $ Data.List.union wrds' lowerCased
+
 addWord :: PermutationMap -> String -> PermutationMap
-addWord graph word = AM.alter graph (insertWord) $ strSort word where
-    lowerCased = strLower word
-    insertWord Nothing = Just [lowerCased]
-    insertWord (Just wrds) = Just $ Data.List.union wrds [lowerCased]
+addWord graph word = addWords graph word [word]
 
 createPermutationMap :: [String] -> PermutationMap
 createPermutationMap = Data.List.foldl' (addWord) empty
