bd477f6c8ba6a37390f6e3267f7837ba274fc40c
Matthew W <matthew@willcockson.family>
2026-01-19 04:36:51 -0600

    fix(nix/nix.dev): add alternative way of specifying channel

--- nix.dev/default.nix
+++ nix.dev/default.nix
@@ -1,13 +1,19 @@
 let
+  /**
+  * this is one way to specify a (somewhat) specific version of nixpkgs
   nixpkgs = builtins.findFile [{
     prefix = "nixpkgs";
     path = "channel:nixos-25.11-small";
   }] "nixpkgs";
+  */
+  # this is an equivalent way
+  nixpkgs = fetchTarball "channel:nixos-25.11";
   pkgs = import nixpkgs {
-    config = { };
-    overlays = [ ];
+    config = {};
+    overlays = [];
   };
   lib = pkgs.lib;
   fs = lib.fileset;
   dir = fs.traceVal ./.;
-in builtins.deepSeq dir dir
+in
+  builtins.deepSeq dir dir
