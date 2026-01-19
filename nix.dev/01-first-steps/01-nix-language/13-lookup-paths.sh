094427e2dc74ea51c850c463a23b75076bb19d24
Matthew W <matthew@willcockson.family>
2026-01-19 15:09:01 -0600

    fix(nix/nix.dev): shell runs differently on different platforms??

--- nix.dev/01-first-steps/01-nix-language/13-lookup-paths.sh
+++ nix.dev/01-first-steps/01-nix-language/13-lookup-paths.sh
@@ -2,4 +2,6 @@
 #! nix-shell -i dash --pure
 #! nix-shell --packages 'nix' 'dash'
 #! nix-shell -I nixpkgs=channel:nixos-25.11-small
+NIX_PATH="$(nix-instantiate --eval --strict --raw ../../default-nixpkgs-url.nix)"
+export NIX_PATH
 nix eval --extra-experimental-features nix-command --impure --expr '[<nixpkgs> <nixpkgs/lib>]'
